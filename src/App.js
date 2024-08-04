8e5895a0009482b3f8246fb0056dadeb5245423a
aqsa <aqsabatool2005@gmail.com>
2024-08-03 18:55:57 -0700

    Cake Selection Page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Navbar from "./components/Navbar";
 //import Card from "./components/Card";
 import CircularCardLayout from "./components/CircularCardLayout";
 import CakeMenu from "./components/CakeMenu";
+import Shop from "./components/Shop";
 //import HomeMenu from "./components/HomeMenu";
 function App() {
   return (
     <>
-      <Navbar />
-      <CakeMenu />
+      <Shop />
     </>
   );
 }
