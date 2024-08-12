1594dc9f0fa141b21a9b7db2b6dbf7071de0826f
aqsa <aqsabatool2005@gmail.com>
2024-08-11 19:38:08 -0700

    Add Banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Banner({ title }) {
   return (
-    <div className="w-[100vw] h-[200px] flex items-center relative bg-black bg-blend-darken bg-opacity-45 mb-12">
+    <div className="w-full h-[200px] flex items-center relative bg-black bg-blend-darken bg-opacity-45 mb-12">
       <div className="container mx-auto">
         <h1 className="text-5xl text-white">{title}</h1>
       </div>
